2fe2a8b9f3d8d45fdf8b24fa9f3ca7fdb0a23b1b
= <=>
2014-03-12 16:19:46 -0500

    pull master

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,11 @@ describe User do
       user.stub(:save).should be_true
     end
 
-
+    it 'Should redirect to homepage' do
+      user.stub(:save).should redirect_to('/')
+      # response.should redirect_to(root)
+      # {get: '/users/sign_up'}.should route_to('home#index')
+    end
   end
 
 end
