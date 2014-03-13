cbfdf88004cb4f4a2cbb891de1633503badeaf22
Daniel Lu <dandydanny@gmail.com>
2014-03-13 09:45:09 -0500

    switching to user branch

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,13 +12,24 @@ describe PostsController do
     expect(post.author).to eq "Steve Dave"
   end
 
-  context "when creating a new post" do
+  # describe "POST /index"
+  # context "when creating a new post" do
+  #   it "persists the post to the database" do
+  #     mock_post = double(:post)
+  #     mock_post.should_receive(:create).and_return(true)
+  #     Post.stub!(:new => mock_post)
+  #     post :index
+  #     expect(response).to redirect_to("posts#index")
+  #   end
+  # end
+  describe "POST /posts/index" do
     it "persists the post to the database" do
       mock_post = double(:post)
       mock_post.should_receive(:create).and_return(true)
-      Post.stub{content => "Here's my post!", author => "Steve Dave"}
-      post :index
+      Post.stub!(:new => mock_post)
+      post :create_post, {}
       expect(response).to redirect_to("posts#index")
+      page.should have_content("Successfully added post.")
     end
   end
 
