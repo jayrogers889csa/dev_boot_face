89f52a72dcb1dbff09a16cf0adf911cd6ebaef8f
jdubnicek <jdubnicek@gmail.com>
2014-03-13 11:17:58 -0500

    create new post view added

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140313145434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "comments", force: true do |t|
     t.text     "content"
