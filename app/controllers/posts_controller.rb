947edd6c67fc9abf95ea641e00b98f60e4c54c6b
jdubnicek <jdubnicek@gmail.com>
2014-03-13 14:33:41 -0500

    add edit post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,10 +25,14 @@ class PostsController < ApplicationController
 
 	def edit
 		@post = Post.where(id: params[:id]).first
-		if @post.update
-			redirect_to posts_path
+	end
+
+	def update
+		@post = Post.where(id: params[:id]).first
+		if @post.update(params[:post].permit(:title, :content))
+			redirect_to post_path
 		else
-			render 'new'
+			render 'edit'
 		end		
 	end
 
